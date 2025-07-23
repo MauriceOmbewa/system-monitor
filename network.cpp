98fc0bcf8859d628115bb72fba19f6c34e1245c9
Maurice <ombewamaurice79@gmail.com>
2025-07-23 11:20:49 +0300

    Implement interface status and type detection functions

--- network.cpp
+++ network.cpp
@@ -77,3 +77,80 @@ string getMacAddress(const string& interface_name) {
     
     return mac_address;
 }
+
+// Check if a network interface is up
+bool isInterfaceUp(const string& interface_name) {
+    bool is_up = false;
+    
+    // Try to read from /sys/class/net/[interface]/operstate
+    string path = "/sys/class/net/" + interface_name + "/operstate";
+    ifstream state_file(path);
+    
+    if (state_file.is_open()) {
+        string state;
+        getline(state_file, state);
+        is_up = (state == "up");
+    } else {
+        // Fallback method using ioctl
+        int fd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (fd != -1) {
+            struct ifreq ifr;
+            memset(&ifr, 0, sizeof(ifr));
+            strncpy(ifr.ifr_name, interface_name.c_str(), IFNAMSIZ - 1);
+            
+            if (ioctl(fd, SIOCGIFFLAGS, &ifr) != -1) {
+                is_up = (ifr.ifr_flags & IFF_UP) && (ifr.ifr_flags & IFF_RUNNING);
+            }
+            
+            close(fd);
+        }
+    }
+    
+    return is_up;
+}
+
+// Get the type of a network interface
+string getInterfaceType(const string& interface_name) {
+    // Check common interface name prefixes to determine type
+    if (interface_name.substr(0, 2) == "lo") {
+        return "Loopback";
+    } else if (interface_name.substr(0, 3) == "eth") {
+        return "Ethernet";
+    } else if (interface_name.substr(0, 4) == "wlan") {
+        return "Wireless";
+    } else if (interface_name.substr(0, 3) == "wlp") {
+        return "Wireless";
+    } else if (interface_name.substr(0, 3) == "enp") {
+        return "Ethernet";
+    } else if (interface_name.substr(0, 3) == "tun") {
+        return "VPN Tunnel";
+    } else if (interface_name.substr(0, 3) == "tap") {
+        return "TAP";
+    } else if (interface_name.substr(0, 4) == "bond") {
+        return "Bond";
+    } else if (interface_name.substr(0, 5) == "bridge") {
+        return "Bridge";
+    } else if (interface_name.substr(0, 4) == "virbr") {
+        return "Virtual Bridge";
+    } else if (interface_name.substr(0, 4) == "docker") {
+        return "Docker";
+    } else {
+        // Try to determine type from /sys/class/net/[interface]/type
+        string path = "/sys/class/net/" + interface_name + "/type";
+        ifstream type_file(path);
+        
+        if (type_file.is_open()) {
+            int type_num;
+            type_file >> type_num;
+            
+            // ARPHRD_ETHER = 1, ARPHRD_LOOPBACK = 772
+            if (type_num == 1) {
+                return "Ethernet";
+            } else if (type_num == 772) {
+                return "Loopback";
+            }
+        }
+        
+        return "Unknown";
+    }
+}
